475383a3591dff7adf826b529bcc43f03f3b1c33
TCostalonga <tarsila.costalonga@gmail.com>
2023-10-30 19:12:53 -0300

    Adding light theme colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.longcoast.vacinacao"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.longcoast.vacinacao"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/longcoast/vacinacao/ui/theme/Color.kt
+++ app/src/main/java/com/longcoast/vacinacao/ui/theme/Color.kt
@@ -2,10 +2,35 @@ package com.longcoast.vacinacao.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/** Light Theme Colors **/
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightPrimary900 = Color(0xFF40036D)
+val LightPrimary800 = Color(0xFF40036D)
+val LightPrimary700 = Color(0xFF55177E)
+val LightPrimary600 = Color(0xFF55177E)
+val LightPrimary500 = Color(0xFF5C1B82)
+val LightPrimary400 = Color(0xFF6D3C8D)
+val LightPrimary300 = Color(0xFF815B9A)
+val LightPrimary200 = Color(0xFF9982A9)
+val LightPrimary100 = Color(0xFFB3AABA)
+
+val LightSecondary900 = Color(0xFFB54100)
+val LightSecondary800 = Color(0xFFBE5903)
+val LightSecondary700 = Color(0xFFC56706)
+val LightSecondary600 = Color(0xFFCB7608)
+val LightSecondary500 = Color(0xFFD0800E)
+val LightSecondary400 = Color(0xFFCF8D29)
+val LightSecondary300 = Color(0xFFD09B4C)
+val LightSecondary200 = Color(0xFFD2B07E)
+val LightSecondary100 = Color(0xFFD2C4AE)
+
+val LightNeutral1000 = Color(0xFF000000)
+val LightNeutral200 = Color(0xFFD0D4D9)
+val LightNeutral100 = Color(0xFFC9CDC6)
+val LightNeutral0 = Color(0xFFFFFFFF)
+
+val LightSurface300 = Color(0xFFF7F9FA)
+val LightSurface200 = Color(0xFFFFFFFF)
+val LightSurface100 = Color(0xFFFFFFFF)
+
+/** Dark Theme Colors **/
